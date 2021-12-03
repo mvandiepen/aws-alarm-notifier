ab2a3d09766407ead973623f135eee705a848448
Mike van Diepen <mvdiepen@zilverline.com>
2021-12-03 13:20:06 +0100

    Use latest AWS provider

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
+      source = "hashicorp/aws"
+      version = "~> 3.68.0"
     }
   }
-
-  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
